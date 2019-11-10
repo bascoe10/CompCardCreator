9d78478107791bdf3f4c960bce5134c958c62411
Abass Sesay <bassco10@gmail.com>
2019-11-09 21:41:45 -0800

    add routing to landing page and theme selecting page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { LandingPageComponent } from "./landing-page/landing-page.component";
+import { ThemeSelectorComponent } from "./theme-selector/theme-selector.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", redirectTo: "/home", pathMatch: "full" },
+  { path: "home", component: LandingPageComponent },
+  { path: "themes", component: ThemeSelectorComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Comp1Component } from './comp1/comp1.component';
 import { Comp2Component } from './comp2/comp2.component';
+import { ThemeSelectorComponent } from './theme-selector/theme-selector.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     Comp1Component,
-    Comp2Component
+    Comp2Component,
+    ThemeSelectorComponent,
+    LandingPageComponent
   ],
   imports: [
     BrowserModule,

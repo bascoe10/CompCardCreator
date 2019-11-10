02eb4e5076f61c25c0430b952e9498960ba94fa9
Abass Sesay <bassco10@gmail.com>
2019-11-10 13:14:21 -0800

    added FormsModule to allow editing comp card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { Comp1Component } from './comp1/comp1.component';
-import { Comp2Component } from './comp2/comp2.component';
-import { ThemeSelectorComponent } from './theme-selector/theme-selector.component';
-import { LandingPageComponent } from './landing-page/landing-page.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { Comp1Component } from "./comp1/comp1.component";
+import { Comp2Component } from "./comp2/comp2.component";
+import { ThemeSelectorComponent } from "./theme-selector/theme-selector.component";
+import { LandingPageComponent } from "./landing-page/landing-page.component";
 
 @NgModule({
   declarations: [
@@ -16,11 +17,8 @@ import { LandingPageComponent } from './landing-page/landing-page.component';
     ThemeSelectorComponent,
     LandingPageComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/comp1/comp1.component.ts
+++ src/app/comp1/comp1.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+
+import { Model } from "../models/model";
 
 @Component({
-  selector: 'app-comp1',
-  templateUrl: './comp1.component.html',
-  styleUrls: ['./comp1.component.sass']
+  selector: "app-comp1",
+  templateUrl: "./comp1.component.html",
+  styleUrls: ["./comp1.component.sass"]
 })
 export class Comp1Component implements OnInit {
+  model = new Model();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }

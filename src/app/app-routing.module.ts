0b9c2e595e4b485f4b3ae262fbac22440e233a0d
Abass Sesay <bassco10@gmail.com>
2019-11-09 22:19:21 -0800

    add link so comp card from theme selector

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { LandingPageComponent } from "./landing-page/landing-page.component";
 import { ThemeSelectorComponent } from "./theme-selector/theme-selector.component";
+import { Comp1Component } from "./comp1/comp1.component";
+import { Comp2Component } from "./comp2/comp2.component";
 
 const routes: Routes = [
   { path: "", redirectTo: "/home", pathMatch: "full" },
   { path: "home", component: LandingPageComponent },
-  { path: "themes", component: ThemeSelectorComponent }
+  { path: "themes", component: ThemeSelectorComponent },
+  { path: "comp1", component: Comp1Component },
+  { path: "comp2", component: Comp2Component }
 ];
 
 @NgModule({
--- src/app/theme-selector/theme-selector.component.ts
+++ src/app/theme-selector/theme-selector.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-theme-selector',
-  templateUrl: './theme-selector.component.html',
-  styleUrls: ['./theme-selector.component.sass']
+  selector: "app-theme-selector",
+  templateUrl: "./theme-selector.component.html",
+  styleUrls: ["./theme-selector.component.sass"]
 })
 export class ThemeSelectorComponent implements OnInit {
+  CompCards = [
+    { title: "Comp 1", name: "comp1", link: "/comp1" },
+    { title: "Comp 2", name: "comp2", link: "/comp2" }
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
